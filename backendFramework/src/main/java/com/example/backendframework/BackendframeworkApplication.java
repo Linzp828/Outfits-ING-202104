bbe1e9e73b6542f3e37544edeb7333de54596e38
Trump800 <60842723+Trump800@users.noreply.github.com>
2021-04-29 19:47:06 +0800

    [Add]吴晗杰：springboot框架

---
+++ backendFramework/src/main/java/com/example/backendframework/BackendframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendframeworkApplication.class, args);
+    }
+
+}
---
+++ backendFramework/src/test/java/com/example/backendframework/BackendframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendframeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
