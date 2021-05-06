a3074ad004f1a2a237611d0b18a7df5281d94066
robot-gif <57623968+robot-gif@users.noreply.github.com>
2021-05-06 23:25:35 +0800

    [Fix]林龙星：根据用户id返回用户信息接口的bug修改

--- backendFramework/src/main/java/com/example/backendframework/Controller/meController/GetUserController.java
+++ backendFramework/src/main/java/com/example/backendframework/Controller/meController/GetUserController.java
@@ -3,6 +3,7 @@ package com.example.backendframework.Controller.meController;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.backendframework.Dao.meDao.GetIntroDao;
+import com.example.backendframework.Dao.meDao.SubscribeDao;
 import com.example.backendframework.Model.User;
 import com.example.backendframework.util.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -20,23 +22,51 @@ public class GetUserController {
     @Autowired
     private GetIntroDao getIntroDao;
 
+    @Autowired
+    private SubscribeDao subscribeDao;
+
     @RequestMapping("getIntro")
     public JSONObject getInfo(@RequestBody JSONObject request){
-        int userId = request.getInteger("userId");
+        int checkedUserId = request.getInteger("userId");
         String token = request.getString("token");
+        boolean isSubscribed = false;
 
         JSONObject response = new JSONObject();
-        response.put("code",200);
-        response.put("msg","操作成功");
 
-        User userInfo =getIntroDao.getIntro(userId,token);
-        Map<String, Object> mapUser = new HashMap<String, Object>();
-        mapUser.put("userNickname",userInfo.getUser_nickname());
-        mapUser.put("userPic",userInfo.getUser_pic_path());
-        mapUser.put("userSex",userInfo.getUser_sex());
-        mapUser.put("userProfile",userInfo.getUser_profile());
+        try {
+            Map<String, Object> code = TokenUtil.parseJWT(token);
+            int userId = Integer.parseInt(code.get("ID").toString());
+            User userInfo =getIntroDao.getIntro(checkedUserId);
+            Map<String, Object> mapUser = new HashMap<>();
+            List<User> subscription = subscribeDao.getSubscription(userId);
+            for (User user : subscription) {
+                if(user.getId()==checkedUserId){
+                    mapUser.put("userState",1);
+                    isSubscribed = true;
+                }
+            }
 
-        response.put("data",mapUser);
+            if(!isSubscribed){
+                mapUser.put("userState",2);
+            }
+            if(userId == checkedUserId){
+                mapUser.put("userState",0);
+            }
+
+            mapUser.put("userNickname", userInfo.getUser_nickname());
+            mapUser.put("userPic", userInfo.getUser_pic_path());
+            mapUser.put("userSex", userInfo.getUser_sex());
+            mapUser.put("userProfile", userInfo.getUser_profile());
+            response.put("data",mapUser);
+            response.put("code",200);
+            response.put("msg","操作成功");
+
+        } catch (NumberFormatException e) {
+            response.put("code",500);
+            response.put("msg","Token错误");
+            response.put("data","");
+        }
+
 
         return response;
     }
--- backendFramework/src/main/java/com/example/backendframework/Dao/meDao/GetIntroDao.java
+++ backendFramework/src/main/java/com/example/backendframework/Dao/meDao/GetIntroDao.java
@@ -4,11 +4,9 @@ import com.example.backendframework.Model.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-import java.util.List;
-
 @Mapper
 public interface GetIntroDao {
     @Select("SELECT * from user WHERE user.id = #{user_id}")
-    User getIntro(int user_id, String user_token);
+    User getIntro(int user_id);
 
 }
