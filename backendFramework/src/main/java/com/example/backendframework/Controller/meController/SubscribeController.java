3819859bbfdae5f41ad7f060230f2f666fda1723
robot-gif <57623968+robot-gif@users.noreply.github.com>
2021-05-06 21:46:55 +0800

    [ADD]林龙星:关注/取消关注用户接口的实现

---
+++ backendFramework/src/main/java/com/example/backendframework/Controller/meController/SubscribeController.java
@@ -0,0 +1,59 @@
+package com.example.backendframework.Controller.meController;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.backendframework.Dao.meDao.SubscribeDao;
+import com.example.backendframework.Model.User;
+import com.example.backendframework.util.TokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class SubscribeController {
+    @Autowired
+    private SubscribeDao subscribeDao;
+
+    @RequestMapping("/subscribe")
+    public JSONObject getSubscription(@RequestBody JSONObject request){
+        int subscribeUserId = request.getInteger("userId");
+        String token = request.getString("token");
+        JSONObject response = new JSONObject();
+        boolean isSubscribed = false;
+
+        try {
+            Map<String, Object> code = TokenUtil.parseJWT(token);
+            int userId = Integer.parseInt(code.get("ID").toString());
+            List<User> subscription = subscribeDao.getSubscription(userId);
+            for (User user : subscription) {
+                if(user.getId()==subscribeUserId){
+                    isSubscribed = true;
+                    subscribeDao.deleteSubscribeUser(userId,subscribeUserId);
+                    response.put("code",200);
+                    response.put("msg","取消关注成功");
+                    response.put("data",null);
+                    break;
+                }
+            }
+            if(!isSubscribed){
+                subscribeDao.addSubscribeUser(userId,subscribeUserId);
+                response.put("code",200);
+                response.put("msg","关注成功");
+                response.put("data",null);
+            }
+
+        } catch (NumberFormatException e) {
+            response.put("code",500);
+            response.put("msg","Token错误");
+            response.put("data","请重新登录");
+        }
+
+        return response;
+    }
+
+}
--- backendFramework/src/main/java/com/example/backendframework/Dao/meDao/SubscribeDao.java
+++ backendFramework/src/main/java/com/example/backendframework/Dao/meDao/SubscribeDao.java
@@ -2,8 +2,7 @@ package com.example.backendframework.Dao.meDao;
 
 
 import com.example.backendframework.Model.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -14,4 +13,12 @@ public interface SubscribeDao {
 
     @Select("select * from user INNER JOIN subscribe_user on user.id=subscribe_user.user_id where subscribe_user.subscribe_user_id=#{user_id};")
     List<User> getSubscriber(int user_id);
+
+    @Insert("insert into subscribe_user(user_id,subscribe_user_id) value(#{user_id}, #{subscribe_user_id}) ")
+    void addSubscribeUser(int user_id,int subscribe_user_id);
+
+    @Delete("delete from subscribe_user where user_id=#{user_id} and subscribe_user_id=#{subscribe_user_id} ")
+    void deleteSubscribeUser(int user_id,int subscribe_user_id);
+
+
 }
