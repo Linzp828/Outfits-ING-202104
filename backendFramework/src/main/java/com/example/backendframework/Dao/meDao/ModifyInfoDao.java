c6556b256ff95c3e4cc2f34820884dc9c5d7e26b
robot-gif <57623968+robot-gif@users.noreply.github.com>
2021-06-15 18:12:07 +0800

    [Style]林龙星：为meDao中的接口添加注释

--- backendFramework/src/main/java/com/example/backendframework/Dao/meDao/ModifyInfoDao.java
+++ backendFramework/src/main/java/com/example/backendframework/Dao/meDao/ModifyInfoDao.java
@@ -5,9 +5,22 @@ import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface ModifyInfoDao {
+
+    /**
+     * @author:  林龙星
+     * @description: 根据传入的信息修改用户信息
+     * @param:  user_id,user_nickname,user_sex,user_profile
+     * @return: 无
+     */
     @Update("UPDATE user SET user_profile = #{user_profile},user_sex = #{user_sex},user_nickname = #{user_nickname} WHERE user.id = #{user_id}")
     void modifyInfo(int user_id,String user_nickname,String user_sex,String user_profile);
 
+    /**
+     * @author:  林龙星
+     * @description: 根据传入的图片设置用户头像
+     * @param:  user_id,user_pic_path
+     * @return: 无
+     */
     @Update("UPDATE user set user_pic_path = #{user_pic_path} where user.id = #{user_id}")
     void setHeadPic(int user_id,String user_pic_path);
 }
--- backendFramework/src/main/java/com/example/backendframework/Dao/meDao/SubscribeDao.java
+++ backendFramework/src/main/java/com/example/backendframework/Dao/meDao/SubscribeDao.java
@@ -8,15 +8,40 @@ import java.util.List;
 
 @Mapper
 public interface SubscribeDao {
+
+    /**
+     * @author:  林龙星
+     * @description: 根据传入的用户id查询用户的关注列表
+     * @param:  user_id
+     * @return: List<User>
+     */
     @Select("select * from user INNER JOIN subscribe_user on user.id=subscribe_user.subscribe_user_id where subscribe_user.user_id=#{user_id};")
     List<User> getSubscription(int user_id);
 
+    /**
+     * @author:  林龙星
+     * @description: 根据传入的用户id查询用户的粉丝列表
+     * @param:  user_id
+     * @return: List<User>
+     */
     @Select("select * from user INNER JOIN subscribe_user on user.id=subscribe_user.user_id where subscribe_user.subscribe_user_id=#{user_id};")
     List<User> getSubscriber(int user_id);
 
+    /**
+     * @author:  林龙星
+     * @description: 根据传入的用户id和要关注的用户的id来添加用户关注关系
+     * @param:  user_id，subscribe_user_id
+     * @return: 无
+     */
     @Insert("insert into subscribe_user(user_id,subscribe_user_id) value(#{user_id}, #{subscribe_user_id}) ")
     void addSubscribeUser(int user_id,int subscribe_user_id);
 
+    /**
+     * @author:  林龙星
+     * @description: 根据传入的用户id和要关注的用户的id来删除用户关注关系
+     * @param:  user_id，subscribe_user_id
+     * @return: 无
+     */
     @Delete("delete from subscribe_user where user_id=#{user_id} and subscribe_user_id=#{subscribe_user_id} ")
     void deleteSubscribeUser(int user_id,int subscribe_user_id);
 
