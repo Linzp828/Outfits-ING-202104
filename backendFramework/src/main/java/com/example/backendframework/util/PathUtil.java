80da65f5434e49251616cc0e1271a0baa7b6ac3d
Lzp <337229927@qq.com>
2021-06-16 18:28:47 +0800

    [Modify]林子鹏：处理图片重命名问题

--- backendFramework/src/main/java/com/example/backendframework/util/PathUtil.java
+++ backendFramework/src/main/java/com/example/backendframework/util/PathUtil.java
@@ -1,24 +1,28 @@
 package com.example.backendframework.util;
-
+import java.util.Date;
+import java.text.SimpleDateFormat;
 public class PathUtil {
     static final String serverBlog = "http://121.5.100.116/static/blogPic/";
     static final String serverHead = "http://121.5.100.116/static/headPic/";
     static final String serverClothing = "http://121.5.100.116/static/clothingPic/";
 
 
-    public static String getBlogPath(){
-        return serverBlog;
+    public static String getBlogPath(String filePath, int userId)
+    {
+        return serverBlog+getDate()+Integer.toString(userId)+filePath;
+
     }
 
-    public static String getHeadPath(){
-        return serverHead;
+    public static String getHeadPath(String filePath, int userId){
+        return serverHead+getDate()+Integer.toString(userId)+filePath;
     }
 
-    public static String getClothingPath(){
-        return serverClothing;
+    public static String getClothingPath(String filePath, int userId){
+        return serverClothing+getDate()+Integer.toString(userId)+filePath;
     }
 
-    public static String getFilePath(String path,String fileName){
-        return path + fileName;
+    public static String getDate(){
+        SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return df.format(new Date());
     }
 }
