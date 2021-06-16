6f2f5235f82905a9e724d1d69286cd95f6e2e385
cj-whales <842111302@qq.com>
2021-06-16 22:54:15 +0800

    [Modify]修改城市名转换脚本

--- backendFramework/src/main/java/com/example/backendframework/util/TransUtil.java
+++ backendFramework/src/main/java/com/example/backendframework/util/TransUtil.java
@@ -5,8 +5,8 @@
  * date: 2021-06-16 21:35
  * copyright(c) 2020-2021 outfits
  */
-package com.example.backendframework.util;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,8 +20,8 @@ import java.io.InputStreamReader;
  * @data: 2021-06-16 21:35
  **/
 public class TransUtil {
-//    public static void main(String[] args) {
-    public static void getLocation(String location) {
+    //    public static void main(String[] args) {
+    public static String getLocation(String location) {
         Process proc;
         try {
             String pyAddr = "E:/GitHub Desktop/Outfits-ING-202104/Python/test2/transfomer.py";
@@ -32,14 +32,24 @@ public class TransUtil {
             BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             String line = null;
             while ((line = in.readLine()) != null) {
-                System.out.println(line);
+//                System.out.println(line);
+                break;
             }
             in.close();
             proc.waitFor();
+            return line;
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        return null;
+//        return line;
+    }
+    //
+    public static void main(String[] args) {
+        String city = getLocation("福州市");
+        System.out.println(city);
     }
 }
